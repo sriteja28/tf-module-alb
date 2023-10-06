2a4a3b083992568198977af465cc0866dfb7830b
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-10-06 10:10:42 +0530

    Updating Terraform files for the project

--- main.tf
+++ main.tf
@@ -36,7 +36,9 @@ resource "aws_lb" "main" {
 resource "aws_lb_listener" "main" {
   load_balancer_arn = aws_lb.main.arn
   port              = var.port
-  protocol          = "HTTP"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:acm:us-east-1:178724175855:certificate/20de0380-7c97-4be2-9e8b-080af278044a"
 
   default_action {
     type = "fixed-response"
