9af72c5092760a18603f3a8ba2faf67d041355fe
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-10-07 11:44:26 +0530

    Updating Terraform files for the project

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "aws_lb" "main" {
 }
 
 resource "aws_lb_listener" "public" {
-  count = var.name == "public" ? 1: 0
+  count = var.name == "public" ? 1 : 0
   load_balancer_arn = aws_lb.main.arn
   port              = "80"
   protocol          = "HTTP"
@@ -58,23 +58,21 @@ resource "aws_lb_listener" "public" {
 }
 
 resource "aws_lb_listener" "private" {
-  count = var.name == "private" ? 1: 0
+  count = var.name == "private" ? 1 : 0
   load_balancer_arn = aws_lb.main.arn
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
     type = "fixed-response"
-  }
 
-  fixed_response {
-    content_type = "text/plain"
-    message_body = "Default Error"
-    status_code  = "500"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Default Error"
+      status_code  = "500"
+    }
   }
 }
-
-
 
 
 resource "aws_lb_listener" "main" {
